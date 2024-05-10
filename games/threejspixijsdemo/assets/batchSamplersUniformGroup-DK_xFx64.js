2bb6f34c63bb1706af99a4f7a66f11253caceea6
skarfroth <mikko.raussi1@gmail.com>
2024-05-10 22:08:53 +0300

    Optimization, borders for latest project

---
+++ games/threejspixijsdemo/assets/batchSamplersUniformGroup-DK_xFx64.js
@@ -0,0 +1,1 @@
+import{M as e}from"./index-Dklv1ufb.js";import{U as o}from"./colorToUniform-BZS1kjX9.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
